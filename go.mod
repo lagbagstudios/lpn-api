24d5720df1eb232fb1938ded78ec505c7295a449
Anders Olson <sredna43@gmail.com>
2026-03-24 20:52:53 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module lpnapi
+
+go 1.25.0
+
+require github.com/tigerbeetle/tigerbeetle-go v0.16.78 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tigerbeetle/tigerbeetle-go"
+)
+
+func main() {
+	fmt.Println("Imported")
+}
